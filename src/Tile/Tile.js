8edb35f948c9096233818af509d3b16e78f178fc
Tyler Rymer <tyler.rymer@gmail.com>
2018-06-21 08:18:42 -0600

    adjusted video, and video function

--- src/Tile/Tile.js
+++ src/Tile/Tile.js
@@ -1,13 +1,11 @@
 import React, { Component, Fragment } from 'react';
 import './Tile.css';
+import video from './videoplayback.mp4';
+import poster from './pres-monson.jpg';
 
 class Tile extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.handleVideoEvent = this.handleVideoEvent.bind(this);
-    // }
-    
     videoClick() {
+        document.getElementById("main_video").play();
         document.getElementById("video_hook").classList.remove("col-md-4");
         document.getElementById("video_hook").classList.add("col-12");
         document.getElementById("video_content").classList.remove("col-md-8");
@@ -22,11 +20,11 @@ class Tile extends Component {
                         <div className="row main-wrapper">
                             <div id="video_hook" className="col-md-4 video-wrapper">
                                 <div className="responsive-video-container">
-                                    <iframe src="https://www.youtube.com/embed/jz9DVB42AA4" className="responsive-video" title="University of Utah Names Building After Thomas S. Monson" allowFullScreen></iframe>
+                                    <video id="main_video" onClick={this.videoClick} poster={poster} controls src={video} type="mp4/video" className="responsive-video"></video>
                                 </div>
                             </div>
                             <div id="video_content" className="col-md-8 inner-content-container">
-                                <h4 onClick={this.videoClick} className="news-release">news release</h4>
+                                <h4 className="news-release">news release</h4>
                                 <h1 className="video-title-tile">University of Utah Names Historic Building After President Monson</h1>
                                 <p className="video-text-snippet">A historic mansion in downtown Salt Lake City has been renamed the Thomas S. Monson Center after the current president of The Church of Jesus Christ of Latter-day Saints.</p>
                             </div>
